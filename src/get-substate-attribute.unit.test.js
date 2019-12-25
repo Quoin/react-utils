3a758f2dadedecf81938e8f41242937639a9c24a
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-12-25 09:20:16 -0500

    - Adding test to make sure default value is returned.

--- src/get-substate-attribute.unit.test.js
+++ src/get-substate-attribute.unit.test.js
@@ -19,9 +19,19 @@ describe(`src/get-substate-attribute`, () => {
         expect(value).to.be.undefined();
     });
 
+    it(`returns default value with empty state`, () => {
+        const value = moduleToTest(EMPTY_STATE, namespace, ATTRIBUTE, 'default-value');
+        expect(value).to.equal('default-value');
+    });
+
     it(`returns nothing if substate has no attributes`, () => {
         const value = moduleToTest(EMPTY_SUBSTATE, namespace, ATTRIBUTE);
         expect(value).to.be.undefined();
+    });
+
+    it(`returns default value if substate has no attributes`, () => {
+        const value = moduleToTest(EMPTY_SUBSTATE, namespace, ATTRIBUTE, 'default-value');
+        expect(value).to.equal('default-value');
     });
 
     it(`returns nothing if attribute was not set`, () => {
