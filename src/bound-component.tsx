f1808a669d07dcafbea728f06e1b2b001ea26fae
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-01-17 10:14:30 -0500

    - #1: Adding boundComponent() and errorBoundary()

--- src/bound-component.tsx
+++ src/bound-component.tsx
@@ -5,13 +5,13 @@ import errorBoundary from './error-boundary';
 import { IGetComponentProps } from './types';
 
 export default (
-  Component: React.Component,
+  Component: React.ComponentClass<any>,
   getComponentProps: IGetComponentProps,
   propTypes: object,
   defaultProps: object
-): React.Component => {
-    const Container = (props: object) => <Component {...getComponentProps(props)} />;
-    Container.displayName = `${baseComponentName(Component.displayName)}Container`;
+): React.ComponentClass => {
+  const Container: React.FunctionComponent<any> = (props: object) => <Component {...getComponentProps(props)} />;
+    Container.displayName = `${baseComponentName(Component)}Container`;
     Container.propTypes = propTypes || Component.propTypes;
     Container.defaultProps = defaultProps || Component.defaultProps;
     return errorBoundary(Container);
