e1533d2556c280426ad075025c85d9334aadcc84
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-01-17 10:14:30 -0500

    - #1: Adding boundComponent() and errorBoundary()

--- src/base-class-component.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class BaseClassComponent extends React.Component<any, any> {
-  static displayName: string;
-  static propTypes: object;
-  static defaultProps: object;
-}
-
-export default BaseClassComponent;
