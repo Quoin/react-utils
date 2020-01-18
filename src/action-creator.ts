4e1337d583466e716593b71060e91e8ee8161cf4
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-01-17 22:07:50 -0500

    - #1: Adding action-creator tests.

--- src/action-creator.ts
+++ src/action-creator.ts
@@ -1,12 +1,6 @@
 import { IAction, IActionType } from './types';
 
-export default (type: IActionType, payload: object): IAction => {
-    if (!type) {
-        throw new Error(`Missing type`);
-    }
-
-    return Object.freeze({
-        type,
-        payload
-    });
-};
+export default (type: IActionType, payload?: object): IAction => Object.freeze({
+    type,
+    payload
+});
