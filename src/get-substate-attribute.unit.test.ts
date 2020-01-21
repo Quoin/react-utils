8bd3b185c052514d5caf10478387b240ee0aae5d
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-01-21 17:06:48 -0500

    - #1: Adding get-substate-attribute tests

--- src/get-substate-attribute.unit.test.ts
+++ src/get-substate-attribute.unit.test.ts
@@ -2,9 +2,10 @@ import { fromJS, Map } from 'immutable';
 
 import moduleToTest from './get-substate-attribute';
 import namespace from './namespace.test';
+import { IState } from './types';
 
 const ATTRIBUTE = 'some-attribute';
-const EMPTY_STATE = Map();
+const EMPTY_STATE = (Map() as IState);
 const EMPTY_SUBSTATE = EMPTY_STATE.set(namespace(), Map());
 const WITHOUT_ATTRIBUTE = EMPTY_STATE.set(namespace(), fromJS({ other: 'attribute' }));
 const WITH_ATTRIBUTE = WITHOUT_ATTRIBUTE.setIn([ namespace(), ATTRIBUTE ], 'something');
@@ -12,11 +13,6 @@ const WITH_ATTRIBUTE = WITHOUT_ATTRIBUTE.setIn([ namespace(), ATTRIBUTE ], 'some
 describe(`src/get-substate-attribute`, () => {
     it(`exposes a function with 4 params`, () => {
         expect(moduleToTest).to.be.a('function').and.to.have.lengthOf(4);
-    });
-
-    it(`returns nothing if empty state`, () => {
-        const value = moduleToTest(EMPTY_STATE, namespace);
-        expect(value).to.be.undefined();
     });
 
     it(`returns default value with empty state`, () => {
