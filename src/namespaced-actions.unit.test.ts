a42a33dd9a4e279a8ae52f75a74ab0396c952a33
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-01-21 17:13:13 -0500

    - #1: Added namespaced-actions tests

--- src/namespaced-actions.unit.test.ts
+++ src/namespaced-actions.unit.test.ts
@@ -3,23 +3,11 @@ import moduleToTest from './namespaced-actions';
 import namespace from './namespace';
 
 const NAMESPACE = namespace('@quoin/react-utils');
-const myNamespace = (key) => `${NAMESPACE}.${key}`;
+const myNamespace = (key?: string): string => `${NAMESPACE}.${key}`;
 
 describe("src/namespaced-actions", () => {
     it("should be a function with 2 params", () => {
         expect(moduleToTest).to.be.a('function').and.have.lengthOf(2);
-    });
-
-    it(`should throw "Missing namespace" when no namespace`, () => {
-        expect(() => moduleToTest()).to.throw(Error, /^Missing namespace$/);
-    });
-
-    it(`should throw "namespace is not a function" when namespace is not a function`, () => {
-        expect(() => moduleToTest("foo")).to.throw(Error, /^namespace is not a function$/);
-    });
-
-    it(`should throw "Missing baseKeys" when no baseKeys`, () => {
-        expect(() => moduleToTest(() => {})).to.throw(Error, /^Missing baseKeys$/);
     });
 
     it(`should return known object`, () => {
