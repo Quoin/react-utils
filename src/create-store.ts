0ae92b51ebaf7db2543c35804c3866795acdbe5c
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-01-22 07:43:42 -0500

    - #1: Trying to get createStore() tests

--- src/create-store.ts
+++ src/create-store.ts
@@ -3,15 +3,15 @@ import { applyMiddleware, compose, createStore, Middleware } from 'redux';
 import { createLogger } from 'redux-logger';
 
 import { INIT_TYPE } from './constants';
-import { IActionType, IState, IReducer } from './types';
+import { IActionType, IReducer, IState, IStore } from './types';
 
 export default (
     reducers: IReducer,
     initialState: IState = Map(),
     middlewares: Middleware<any, any, any>[],
     inDevelopment: boolean,
-    projectInitType: IActionType
-) => {
+    projectInitType?: IActionType
+): IStore => {
     let composeEnhancers = compose;
 
     const clonedMiddlewares = [ ...middlewares ];
