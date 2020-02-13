b726ba4ebfbdf5b30f8f0d4d41fe16fb5838c2eb
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-02-13 15:39:49 -0500

    - #1: Fixing guard-action tests.

--- src/guard-action.ts
+++ src/guard-action.ts
@@ -1,13 +1,9 @@
 import { IAction, IReducer, IState } from './types';
 
 export default (reducer?: IReducer, actionTypes?: string[]) => (state: IState, action: IAction) => {
-    if (!actionTypes) {
+    if (!reducer || !actionTypes || (actionTypes.indexOf(action.type) === -1)) {
       return state;
-    } else if (actionTypes.indexOf(action.type) === -1) {
-        return state;
-    } else if (reducer) {
-        return reducer(state, action);
     } else {
-      return state;
+      return reducer(state, action);
     }
 };
--- src/guard-action.unit.test.ts
+++ src/guard-action.unit.test.ts
@@ -6,6 +6,7 @@ import { IAction, IState, IReducer } from './types';
 import moduleToTest from './guard-action';
 import { DEFAULT_STATE } from './constants'
 
+const INITIAL_STATE = fromJS({ some: 'thing' });
 const TEST_ACTION = `GUARD_ACTION_TYPE`;
 const TEST_ATTRIBUTE = `GUARD_ACTION_ATTRIBUTE`;
 const TEST_VALUE = fromJS({ 'GUARD_ACTION_VALUE': 'GUARD_ACTION_VALUE' });
@@ -13,17 +14,37 @@ const TEST_VALUE = fromJS({ 'GUARD_ACTION_VALUE': 'GUARD_ACTION_VALUE' });
 const TEST_REDUCER: IReducer = (state: IState = DEFAULT_STATE, action: IAction): IState => state.set(TEST_ATTRIBUTE, TEST_VALUE);
 
 describe(`src/guard-action`, () => {
-    it(`calls the reducer when defined in list`, () => {
-        const reducer = moduleToTest(TEST_REDUCER, [ 'foo', TEST_ACTION, 'bar' ]);
-        const state = reducer(fromJS({}), actionCreator(TEST_ACTION));
-        expect(state).to.equal(fromJS({
-            [TEST_ATTRIBUTE]: TEST_VALUE
-        }));
+    it(`returns initial state when no reducer.`, () => {
+        const reducer = moduleToTest();
+        const state = reducer(INITIAL_STATE, actionCreator(TEST_ACTION));
+        expect(state).to.equal(INITIAL_STATE);
+    });
+
+    it(`returns initial state when no actionTypes`, () => {
+        const reducer = moduleToTest(TEST_REDUCER);
+        const state = reducer(INITIAL_STATE, actionCreator(TEST_ACTION));
+        expect(state).to.equal(INITIAL_STATE);
+    });
+
+    it(`returns initial state when empty actionTypes`, () => {
+        const reducer = moduleToTest(TEST_REDUCER, []);
+        const state = reducer(INITIAL_STATE, actionCreator(TEST_ACTION));
+        expect(state).to.equal(INITIAL_STATE);
     });
 
     it(`doesn't call reducer when not in list`, () => {
         const reducer = moduleToTest(TEST_REDUCER, [ 'foo', 'bar' ]);
-        const state = reducer(fromJS({}), actionCreator(TEST_ACTION));
-        expect(state).to.equal(fromJS({}));
+        const state = reducer(INITIAL_STATE, actionCreator(TEST_ACTION));
+        expect(state).to.equal(fromJS({ some: 'thing' }));
+    });
+
+    it(`calls the reducer when defined in list`, () => {
+        const reducer = moduleToTest(TEST_REDUCER, [ 'foo', TEST_ACTION, 'bar' ]);
+
+        const state = reducer(INITIAL_STATE, actionCreator(TEST_ACTION));
+        expect(state).to.equal(fromJS({
+          ...INITIAL_STATE.toJS(),
+          [TEST_ATTRIBUTE]: TEST_VALUE
+        }));
     });
 });
