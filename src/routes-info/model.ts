09a2f1cb72c576dfc0a03cc348a0b5fbedaf43a7
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-02-27 15:12:13 -0500

    - Adding typescript-eslint. Running --fix

--- src/routes-info/model.ts
+++ src/routes-info/model.ts
@@ -6,33 +6,33 @@ import { IRoute } from './types';
 class RoutesInfo {
   _routes: IRoute[];
 
-    constructor() {
-        this._routes = [];
-    }
+  constructor() {
+      this._routes = [];
+  }
 
-    configure(definitions: IRoute[]) {
-        this._routes = generateRoutes(definitions, []);
-    }
+  configure(definitions: IRoute[]) {
+      this._routes = generateRoutes(definitions, []);
+  }
 
-    register(app: express.Application, controller: express.RequestHandler) {
-        this._routes.forEach((route) => {
-            app.get(route.route, controller);
-        });
-    }
+  register(app: express.Application, controller: express.RequestHandler) {
+      this._routes.forEach((route) => {
+          app.get(route.route, controller);
+      });
+  }
 
-    to(name: string, params: { [index: string]: any }) {
-        const foundRoute = this._routes.find((route) => route.name === name);
-        if (foundRoute) {
-            return foundRoute.template.expand(params);
-        } else {
-            return '';
-        }
-    }
+  to(name: string, params: { [index: string]: any }) {
+      const foundRoute = this._routes.find((route) => route.name === name);
+      if (foundRoute) {
+          return foundRoute.template.expand(params);
+      } else {
+          return '';
+      }
+  }
 
-    path(name: string) {
-        const foundRoute = this._routes.find((route) => route.name === name);
-        return foundRoute ? foundRoute.route : '';
-    }
+  path(name: string) {
+      const foundRoute = this._routes.find((route) => route.name === name);
+      return foundRoute ? foundRoute.route : '';
+  }
 }
 
 export default RoutesInfo;
