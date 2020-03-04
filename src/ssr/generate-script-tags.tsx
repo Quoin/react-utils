37455d7f9f8c3457c2f80b366af0bca744f6cd38
Hữu Ðà Trần <huuda.tran@quoininc.com>
2020-03-04 15:49:49 -0500

    - #11: Refactoring ssr page assets.

---
+++ src/ssr/generate-script-tags.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import {
+    AssetType,
+    AssetTypes,
+    ExternalScriptType,
+    ScriptType
+} from './types';
+
+export default (assets: readonly AssetType[] = []): React.ReactNode[] => assets
+    .filter((asset) => asset.type === AssetTypes.SCRIPT)
+    .map((asset, index) => {
+        const script : ScriptType = (asset as ScriptType);
+        if (script.inline) {
+            return false;
+        } else {
+            const externalScript : ExternalScriptType = (script as ExternalScriptType);
+            return (
+                <script key={index} type="text/javascript" async={externalScript.async} src={externalScript.src}></script>
+            );
+        }
+    })
+;
